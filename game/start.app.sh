f64ee6c5cbd471b8e7b2101ab2bf264fbcc22f08
Mutalip <jokermt2357@gmail.com>
2025-12-16 16:37:01 +0600

    Update start.app.sh

--- game/start.app.sh
+++ game/start.app.sh
@@ -5,7 +5,7 @@ set -o pipefail
 set -o nounset
 set -e
 
-echo "🔄 Running migrations..."
+echo "Running migrations..."
 alembic upgrade head
 
 export GUNICORN_CMD_ARGS="${GUNICORN_CMD_ARGS:-"-b 0.0.0.0:8000 --timeout 30 --graceful-timeout 30 --forwarded-allow-ips=* --max-requests=10000 --chdir=/app"}"
